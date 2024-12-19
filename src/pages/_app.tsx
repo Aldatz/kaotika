9aa3ecb7a501561101aede2a74c4696ee069509a
Aldatz <aldatz.frutos@ikasle.aeg.eus>
2024-12-19 13:17:56 +0100

    prueva de despliege

---
+++ src/pages/_app.tsx
@@ -0,0 +1,36 @@
+import { SessionProvider } from "next-auth/react";
+import { AppProps } from 'next/app';
+import "../styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react";
+import Head from "next/head";
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const mockSession = {
+    user: {
+      name: "ENEKO LARREA PEREZ",
+      email: "eneko.larrea@ikasle.aeg.eus",
+      image: "",
+    },
+    expires: "2025-12-31T23:59:59.999",
+    accessToken: "",
+    refreshToken: "",
+    email: "eneko.larrea@ikasle.aeg.eus",
+  };
+
+  return (
+    <SessionProvider session={mockSession}>
+      <NextUIProvider>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </SessionProvider>
+  );
+}
+
+export default MyApp;
